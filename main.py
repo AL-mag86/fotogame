09b7c771d2fba69ff1c5f752ee715605b1709a89
AL-mag86 <al2011_86@inbox.ru>
2026-01-13 19:36:04 +0300

    change 2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def change_photo():
         img_p= PhotoImage(file=dir_animal+anim[i]).subsample(4,4)
         but.configure(image=img_p)
         but.image=img_p
-        but.grid(column=i, row=1, padx=5)
+        but.grid(column=i, row=1, padx=4)
 
    
         
@@ -21,7 +21,7 @@ def change_photo():
         
 tk = Tk()
 tk.title('Обучение по картинкам')
-tk.geometry('1240x600')
+tk.geometry('1245x600')
 tk.resizable(0,0)
 
 dir_animal='C:\\PyProject\\test\\alenv\\animal\\'
@@ -40,22 +40,22 @@ lab_anim0=Button(image=img0, highlightthickness=0)
 lab_anim0.image=img0
 lab_anim0.bind("<Enter>", lambda event:lab_anim0.configure(background='red'))
 lab_anim0.bind("<Leave>", lambda event:lab_anim0.configure(background='lightgray'))
-lab_anim0.grid(column=0, row=1, padx=5)
+lab_anim0.grid(column=0, row=1, padx=3)
 
 img1= PhotoImage(file=dir_animal+animal_list[1]).subsample(4,4)
 lab_anim1=Button(image=img1, highlightthickness=0)
 lab_anim1.image=img1
-lab_anim1.grid(column=1, row=1, padx=5)
+lab_anim1.grid(column=1, row=1, padx=3)
    
 img2= PhotoImage(file=dir_animal+animal_list[2]).subsample(4,4)
 lab_anim2=Button(image=img2, highlightthickness=0)
 lab_anim2.image=img2
-lab_anim2.grid(column=2, row=1, padx=5)
+lab_anim2.grid(column=2, row=1, padx=3)
   
 img3= PhotoImage(file=dir_animal+animal_list[3]).subsample(4,4)
 lab_anim3=Button(image=img3, highlightthickness=0)
 lab_anim3.image=img3
-lab_anim3.grid(column=3, row=1, padx=5)
+lab_anim3.grid(column=3, row=1, padx=3)
 lbl_find.configure(text=find_photo.replace(".png",""))
     #next_btn.configure(text="Следующий слайд")
 
@@ -71,7 +71,7 @@ all_button=[lab_anim0,lab_anim1,lab_anim2,lab_anim3]
 
 
 
-next_btn=Button(text="начать", font='Gabriola 15', command=change_photo)
+next_btn=Button(text="далее", font='Gabriola 15', command=change_photo)
 next_btn.grid(column=0, row=2)
 
 tk.mainloop()
