01d94b977f957a02cee5d654a3b1a339be129870
AL-mag86 <al2011_86@inbox.ru>
2026-01-15 22:03:57 +0300

    coorect logic an visual effect

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from os import path
 import os
 from PIL import Image, ImageTk
 import random
+import time
 #цикл обработки нажатия кнопки следующее фото для смены картинок
 def change_photo():
     anim=random.sample(list_animal, 4)
@@ -15,19 +16,19 @@ def change_photo():
         #but.grid(column=i, row=1, padx=4)    
         lbl_find.configure(text=find_photo.replace(".png",""))  
     global animal_list, target_photo
-    animal_list=anim[:]
-    target_photo=find_photo[:]
-    print(animal_list)
-    print(anim)
-    print(target_photo)
-    print(find_photo)
+    animal_list=anim
+    target_photo=find_photo
+    
 
 def find_img (list,foto):
-    #print(animal_list)
-    #print(target_photo)
     if list==foto:
-        print('нашли')
-    else: print('не нашли')
+        tk.configure(background='green')
+        tk.after(200,lambda: tk.configure(background='SystemButtonFace'))
+        
+    else:
+        tk.configure(background='red')
+        tk.after(200,lambda: tk.configure(background='SystemButtonFace'))
+        
         
 tk = Tk()
 tk.title('Обучение по картинкам')
@@ -48,29 +49,29 @@ lbl_find.grid(column=1, row=0)
 img0= PhotoImage(file=directory+animal_list[0]).subsample(4,4)
 lab_anim0=Button(image=img0, highlightthickness=0)
 lab_anim0.image=img0
-#lab_anim0.bind("<Enter>", lambda event:lab_anim0.configure(background='red'))
-#lab_anim0.bind("<Leave>", lambda event:lab_anim0.configure(background='lightgray'))
+lab_anim0.bind("<Enter>", lambda event:lab_anim0.configure(background='red'))
+lab_anim0.bind("<Leave>", lambda event:lab_anim0.configure(background='lightgray'))
 lab_anim0.grid(column=0, row=1, padx=3)
 
 img1= PhotoImage(file=directory+animal_list[1]).subsample(4,4)
 lab_anim1=Button(image=img1, highlightthickness=0)
 lab_anim1.image=img1
-#lab_anim1.bind("<Enter>", lambda event:lab_anim1.configure(background='red'))
-#lab_anim1.bind("<Leave>", lambda event:lab_anim1.configure(background='lightgray'))
+lab_anim1.bind("<Enter>", lambda event:lab_anim1.configure(background='red'))
+lab_anim1.bind("<Leave>", lambda event:lab_anim1.configure(background='lightgray'))
 lab_anim1.grid(column=1, row=1, padx=3)
    
 img2= PhotoImage(file=directory+animal_list[2]).subsample(4,4)
 lab_anim2=Button(image=img2, highlightthickness=0)
 lab_anim2.image=img2
-#lab_anim2.bind("<Enter>", lambda event:lab_anim2.configure(background='red'))
-#lab_anim2.bind("<Leave>", lambda event:lab_anim2.configure(background='lightgray'))
+lab_anim2.bind("<Enter>", lambda event:lab_anim2.configure(background='red'))
+lab_anim2.bind("<Leave>", lambda event:lab_anim2.configure(background='lightgray'))
 lab_anim2.grid(column=2, row=1, padx=3)
   
 img3= PhotoImage(file=directory+animal_list[3]).subsample(4,4)
 lab_anim3=Button(image=img3, highlightthickness=0)
 lab_anim3.image=img3
-#lab_anim3.bind("<Enter>", lambda event:lab_anim3.configure(background='red'))
-#lab_anim3.bind("<Leave>", lambda event:lab_anim3.configure(background='lightgray'))
+lab_anim3.bind("<Enter>", lambda event:lab_anim3.configure(background='red'))
+lab_anim3.bind("<Leave>", lambda event:lab_anim3.configure(background='lightgray'))
 lab_anim3.grid(column=3, row=1, padx=3)
 lbl_find.configure(text=target_photo.replace(".png",""))
 
